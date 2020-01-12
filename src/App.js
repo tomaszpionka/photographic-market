95a2e0c351333a0febe6dbdca1ca012f5e50c53a
tomaszpionka <tomasz.pionka@protonmail.com>
2020-01-12 17:14:39 +0100

    Added few pages

--- src/App.js
+++ src/App.js
@@ -7,13 +7,33 @@ import {
     Route,
     Link
 } from "react-router-dom";
+//
+// function TestHeader() {
+//     return <h1>Test</h1>;
+// }
+
+const Test = () => (
+    <div>
+
+        Welcome to the Test route
+
+        <Route exact path={'/test'}>
+            <h1>Test number 1</h1>
+        </Route>
+
+        <Route exact path={'/test/2'}>
+            <h2>Test number 2</h2>
+        </Route>
+    </div>
+);
 
 function App() {
   return (
       <Router>
           <header>
               <Link to={'/'}>To nothing</Link>
-              <Link to={'/test'}>To test</Link>
+              <Link to={'/test'}>To the test</Link>
+              <Link to={'/test/2'}>To the test number 2</Link>
           </header>
 
           <main>
@@ -21,9 +41,7 @@ function App() {
                   <Route exact path={'/'}>
                   <h1>Nothing</h1>
                   </Route>
-                  <Route exact path={'/test'}>
-                      <h1>Test</h1>
-                  </Route>
+                  <Route exact path={'/test'} component={Test}/>
               </Switch>
           </main>
 
