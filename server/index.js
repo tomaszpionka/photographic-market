fd4109b783f9a11c5c7aeebc55dd638c41836578
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2020-03-15 14:37:15 +0100

    server setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+// const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.json())
+// app.use(cors());
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use('/auth', authRoutes);
+
+app.use(errorController.get404);
+
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`);
+})
