d7cd0b2849c98af6c1502ec3f5dbb9eaab63b818
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2020-03-17 21:47:08 +0100

    index updated

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const router = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json({type: '*/*'}))
 app.use(
