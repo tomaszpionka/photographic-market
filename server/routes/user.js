c7d80ebbc32bfeab1a96658c7961adaa8369b028
jmczerwinska <asia.frej@gmail.com>
2020-04-01 18:55:06 +0200

    add user finding route

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/user');
 const bodyParser = require('body-parser');
 
-router.use('/', bodyParser.json(), userController.getAllUsers);
+router.get('/', bodyParser.json(), userController.getAllUsers);
+router.get('/find', bodyParser.json(), userController.findUser);
 
 module.exports = router;
