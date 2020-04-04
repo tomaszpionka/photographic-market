1faf8020cf2e7cfd2de2d045fc60ec07f0822bea
jmczerwinska <asia.frej@gmail.com>
2020-04-04 11:16:45 +0200

    add search filter component

--- client/src/components/items/ItemSearchFilter.js
+++ client/src/components/items/ItemSearchFilter.js
@@ -4,16 +4,18 @@ import { Form } from 'semantic-ui-react';
 function SearchFilter() {
     return (
         <Form onSubmit={search} style={{ margin: '0 auto', width: '40%' }}>
-            <Form.Input
-                type="text"
-                placeholder="Search user..."
-                onChange={e => setQuery(e.target.value)}
-                action={{
-                    type: 'submit',
-                    icon: 'search',
-                    color: 'primary',
-                    content: 'Search'
-                }} />
+            <Form.Input action >
+                <input
+                    type="text"
+                    placeholder="Search user..."
+                    onChange={e => setQuery(e.target.value)} />
+                <Form.Select />
+                <Form.Button
+                    type="submit"
+                    icon="search"
+                    primary
+                    content="Search" />
+            </Form.Input>
             <Form.Dropdown />
         </Form>
 
