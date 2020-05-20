365c9279423232c7d7c11b1ddaa51bc7dd146d80
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2020-05-20 23:37:43 +0200

    clean comments

--- client/src/components/items/Items.js
+++ client/src/components/items/Items.js
@@ -1,39 +1,14 @@
-import React, { useEffect, useState, Fragment } from "react";
-import { Container, Header, Form, Button, Item } from "semantic-ui-react";
-// import ItemSearchFilter from "./ItemSearchFilter";
-// import ItemsForm from "./ItemsForm";
-// import AddItemForm from "../AddItemForm";
+import React, { Fragment } from "react";
+import { Container, Header } from "semantic-ui-react";
 import ItemsList from "./ItemsList";
 import ItemsSearch from "./ItemsSearch";
 
 const Items = () => {
-  // const [id, setId] = useState("");
-
-  // const getProfile = async () => {
-  //   try {
-  //     const res = await fetch("http://localhost:5000/dashboard", {
-  //       method: "GET",
-  //       headers: { jwt_token: localStorage.token },
-  //     });
-  //     const parseData = await res.json();
-  //     setName(parseData[0].user_name);
-  //     setId(parseData[0].user_id);
-  //   } catch (err) {
-  //     console.error(err.message);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getProfile();
-  // }, []);
-
   return (
     <Fragment>
       <Container style={{ marginTop: "32px" }}>
         <Container text style={{ marginTop: "7em" }}>
-          {/* <ItemSearchFilter /> */}
           <Header as="h1">items</Header>
-
           <p>this is a basic item market</p>
           <p>user or a visitor can see the content freely</p>
         </Container>
