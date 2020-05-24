e1192216dd19b914a0fb98b011789ee4f4f66ffe
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2020-05-24 18:45:07 +0200

    update user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,5 @@
 const User = require("../models/user");
 const { sequelize, Sequelize } = require("../database/db");
-const Op = Sequelize.Op;
 
 const getAllUsers = (req, res) => {
   return User.findAll()
@@ -10,21 +9,14 @@ const getAllUsers = (req, res) => {
 
 const findUser = (req, res) => {
   const { name } = req.query;
-  return (
-    sequelize
-      .query(
-        `SELECT * FROM users WHERE user_name || ' ' || user_surname ILIKE '%${name}%'`
-      )
-      // return (
-      //   User.findOne({ where: { user_id: { [Op.like]: `%${name}%` } } })
-      // return User.findOne({ where: { user_id: `${name}` } })
-      .then((result) => {
-        console.log(result);
-        // res.send(result[0]);
-        res.json(result);
-      })
-      .catch((error) => res.send(error))
-  );
+  return sequelize
+    .query(
+      `SELECT * FROM users WHERE user_name || ' ' || user_surname ILIKE '%${name}%'`
+    )
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((error) => res.send(error));
 };
 
 module.exports = { getAllUsers, findUser };
