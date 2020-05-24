4e183adfc87ed230d810fcdb6bc61d15342a52cb
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2020-05-24 18:45:42 +0200

    clean auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,82 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
-const db = require("../database/db");
-const { QueryTypes } = require("sequelize");
-const validInfo = require("../middleware/validInfo");
-const jwtGenerator = require("../utils/jwtGenerator");
-const authorize = require("../middleware/authorize");
 const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const validInfo = require("../middleware/validInfo");
+const authorize = require("../middleware/authorize");
 
-router.post("/register", bodyParser.json(), validInfo, async (req, res) => {
-  const { email, name, surname, password, phone, city } = req.body;
+const authController = require("../controllers/auth");
 
-  try {
-    const user = await db.sequelize.query(
-      `SELECT * FROM users WHERE user_email = '${email}'`,
-      {
-        type: QueryTypes.SELECT,
-      }
-    );
+router.post("/register", jsonParser, validInfo, authController.register);
 
-    if (user.length > 0) {
-      return res.status(401).json("user already exist!");
-    }
+router.post("/login", jsonParser, validInfo, authController.login);
 
-    const salt = await bcrypt.genSalt(10);
-    const bcryptPassword = await bcrypt.hash(password, salt);
-
-    let newUser = await db.sequelize.query(
-      `INSERT INTO users (user_name, user_surname, user_email, user_password, user_phone, user_city) VALUES ('${name}','${surname}','${email}','${bcryptPassword}', '${phone}','${city}') RETURNING *`,
-      {
-        type: QueryTypes.SELECT,
-      }
-    );
-
-    const jwtToken = jwtGenerator(newUser[0].user_id);
-
-    return res.json({ jwtToken });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("server error");
-  }
-});
-
-router.post("/login", bodyParser.json(), validInfo, async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await db.sequelize.query(
-      `SELECT * FROM users WHERE user_email = '${email}'`,
-      {
-        type: QueryTypes.SELECT,
-      }
-    );
-
-    if (user.length == 0) {
-      return res.status(401).json("invalid credential");
-    }
-
-    const validPassword = await bcrypt.compare(password, user[0].user_password);
-
-    if (!validPassword) {
-      return res.status(401).json("invalid credential");
-    }
-    const jwtToken = jwtGenerator(user[0].user_id);
-    return res.json({ jwtToken });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("server error");
-  }
-});
-
-router.post("/verify", authorize, (req, res) => {
-  try {
-    res.json(true);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("server error");
-  }
-});
+router.post("/verify", authorize, authController.verify);
 
 module.exports = router;
