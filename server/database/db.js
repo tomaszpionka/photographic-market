e73b1e58c4de4dcf6f4c29435d1948a01eb3e7f5
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2020-05-24 18:45:19 +0200

    disable sequelize logs

--- server/database/db.js
+++ server/database/db.js
@@ -1,21 +1,26 @@
-const Sequelize = require('sequelize');
-const config = require('../config');
+const Sequelize = require("sequelize");
+const config = require("../config");
 
-const db = {}
+const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
     dialect: config.dialect,
     host: config.host,
-
+    logging: false,
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
