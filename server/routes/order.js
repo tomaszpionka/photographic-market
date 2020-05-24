40332b8731b601f628614aa1f51af939024cae8b
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2020-05-24 18:03:44 +0200

    add order status control

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = express.Router();
-// const userController = require("../controllers/user");
 const bodyParser = require("body-parser");
 const db = require("../database/db");
 const { QueryTypes } = require("sequelize");
 const Order = require("../models/order");
+const authorize = require("../middleware/authorize");
 
 router.get("/", bodyParser.json(), async (req, res) => {
   try {
     const orders = await db.sequelize.query(`SELECT * FROM orders`, {
       type: QueryTypes.SELECT,
     });
-    console.log(orders);
     res.json(orders);
   } catch (error) {
     console.error(error.message);
@@ -23,15 +22,23 @@ router.post(
   bodyParser.json(),
   async (req, res) => {
     try {
-      console.log(req.params);
       const { item_id, item_owner, item_buyer } = req.params;
+      const order = await db.sequelize.query(
+        `SELECT * FROM orders WHERE item_id = '${item_id}' AND item_buyer = '${item_buyer}'`,
+        {
+          type: QueryTypes.SELECT,
+        }
+      );
+      if (order.length > 0) {
+        return res.status(401).json("order already exist!");
+      }
       Order.create({
         item_id: item_id,
         item_owner: item_owner,
         item_buyer: item_buyer,
+        order_init: true,
       })
         .then((result) => {
-          console.log(result);
           res.send({ result });
         })
         .catch((error) => {
@@ -42,4 +49,63 @@ router.post(
     }
   }
 );
+
+router.put(
+  "/offer/:order_id/:item_id/:process",
+  authorize,
+  bodyParser.json(),
+  async (req, res) => {
+    try {
+      const { order_id, item_id, process } = req.params;
+      if (process === "true") {
+        const offer = await db.sequelize.query(
+          `SELECT * from orders WHERE item_id = '${item_id}' and order_process = 'true'`,
+          {
+            type: QueryTypes.SELECT,
+          }
+        );
+        if (offer.length > 0) {
+          return res.status(401).json("offer already confirmed");
+        } else {
+          const updateOffer = await db.sequelize.query(
+            `UPDATE orders SET order_process = '${process}' WHERE order_id = '${order_id}' RETURNING *`
+          );
+
+          if (updateOffer[0].length === 0) {
+            return res.json("this offer is not yours");
+          }
+          res.json(updateOffer[0]);
+        }
+      } else {
+        const updateOffer = await db.sequelize.query(
+          `UPDATE orders SET order_process = '${process}' WHERE order_id = '${order_id}' RETURNING *`
+        );
+        if (updateOffer[0].length === 0) {
+          return res.json("this offer is not yours");
+        }
+        res.json(updateOffer[0]);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+);
+
+router.put(
+  "/confirm/:order_id",
+  authorize,
+  bodyParser.json(),
+  async (req, res) => {
+    try {
+      const { order_id } = req.params;
+      const confirmOrder = await db.sequelize.query(
+        `UPDATE orders SET order_success = 'true' WHERE order_id = '${order_id}' RETURNING *`
+      );
+      res.json(confirmOrder[0]);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+);
+
 module.exports = router;
