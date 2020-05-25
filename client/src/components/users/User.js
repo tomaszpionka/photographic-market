26f0468831f195dcbd0ff170698a98ca50b935f6
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2020-05-25 17:32:54 +0200

    add user card and edition

--- client/src/components/users/User.js
+++ client/src/components/users/User.js
@@ -1,28 +1,24 @@
 import React, { useEffect, useState, Fragment } from "react";
 import {
   Item,
-  Image,
   Container,
   Header,
   Button,
+  Card,
+  Image,
   Icon,
   Segment,
-  Modal,
 } from "semantic-ui-react";
 
-const User = ({ user_id }) => {
-  const [query, setQuery] = useState([]);
-  const onSubmitForm = async (e) => {
-    try {
-      const response = await fetch(
-        `http://localhost:5000/users/query/?name=${user_id}`
-      );
-      const parsedResponse = await response.json();
-      setQuery(parsedResponse);
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
+import UserEdit from "./UserEdit";
+
+const User = ({ user, setUsersChange }) => {
+  const [userData, setUserData] = useState(user);
+  console.log(userData.length);
+
+  useEffect(() => {
+    setUserData(user);
+  }, [user]);
 
   return (
     <Fragment>
@@ -32,37 +28,32 @@ const User = ({ user_id }) => {
       </Header>
       <Segment attached>
         <Container>
-          <Button onClick={onSubmitForm} primary>
-            show
-          </Button>
-
-          <Item.Group divided>
-            {query.length !== 0 &&
-              query[0].user_id !== null &&
-              query.map((user) => (
-                <Item key={user.user_id}>
-                  <Item.Image src={user.user_image} />
-                  <Item.Content>
-                    <Item.Header as="a">
-                      {user.user_name} {user.user_surname}
-                    </Item.Header>
-                    <Item.Meta>
-                      <span>
-                        {user.createdAt.slice(0, 10)}{" "}
-                        {user.createdAt.slice(11, 16)}
-                      </span>
-                      <br />
-                      <span>{user.user_email}</span>
-                      <br />
-                      <span>{user.user_phone}</span>
-                      <br />
-                      <span>{user.user_city}</span>
-                    </Item.Meta>
-                    <Item.Description>{user.item_description}</Item.Description>
-                  </Item.Content>
-                </Item>
-              ))}
-          </Item.Group>
+          <Card>
+            <Image src={userData.user_image} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>
+                {userData.user_name} {userData.user_surname}
+              </Card.Header>
+              <Card.Meta>
+                <span className="meta">{userData.user_email}</span>{" "}
+                <span className="meta">{userData.user_phone}</span>
+              </Card.Meta>
+              <Card.Description>
+                {userData.user_name} is currently in {userData.user_city}.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <UserEdit
+                userData={userData}
+                setUsersChange={setUsersChange}
+                inline
+              />
+              {/* <span>
+                <Icon name="user" />
+                22 Friends
+              </span> */}
+            </Card.Content>
+          </Card>
         </Container>
       </Segment>
     </Fragment>
